57010b4b18e2fe10862d68f493d219c67a51b19f
Aditya <adityachouhan929@gmail.com>
2026-08-26 18:38:41 +0530

    db logs and migration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
+import { connectDB } from "./config/db.ts"
+
 const app = express();
 
-
+connectDB();
 
 export default app;
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,20 @@ import { PrismaClient } from '../generated/prisma/client.ts'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL!
 })
 
 export const prisma = new PrismaClient({ adapter })
+
+
+export async function connectDB() {
+    try {
+        await prisma.$connect();
+        console.log("Database Connected Successfully");
+        
+    }catch(error) {
+        console.error("Database Connection Failed ", error);
+        process.exit(1);
+    }
+}
+    
