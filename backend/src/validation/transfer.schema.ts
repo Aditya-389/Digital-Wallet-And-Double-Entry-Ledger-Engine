f032c2f0071831435d81ea66191239a5fd10d034
Aditya <adityachouhan929@gmail.com>
2026-09-03 20:13:10 +0530

    schema for transfer

---
+++ backend/src/validation/transfer.schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+
+/*
+Not accepting sourceWalletId from the client.
+The source wallet should come from the authenticated user's context
+
+E.g., sourceWallet.userId === req.userId
+
+NOTE: 
+
+1. The regex string represents a "positive" whole number or a decimal number with up to 4 decimal places.
+2. Limits the whole number part to a maximum of 15 digits, cause my DB constrain allows upto 15 digits
+
+*/
+export const transferSchema = z.object({
+    destinationWalletId: z.number().int().positive(),
+    amount: z.string().regex(/^\d{1,15}(\.\d{1,4})?$/), // taking amount in string
+}).strict();
+
+
+export type transferSchemaInput = z.infer<typeof transferSchema>;
